4e5cb205f2b3db5bea78e9b9728681c2f3d5f019
Andreas Kroll <andreas.kroll@yahoo.de>
2017-03-06 13:50:18 +0100

    Create langconfig.php

---
+++ system/config/langconfig.php
@@ -0,0 +1,15 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['news'] = array('Blog');
+//$GLOBALS['TL_LANG']['MOD']['faq'] = array('Häufige Fragen');
+$GLOBALS['TL_LANG']['MSC']['com_submit'] = 'Absenden';
+$GLOBALS['TL_LANG']['MSC']['com_email'] = 'E-Mail';
+$GLOBALS['TL_LANG']['MSC']['copyOf'] = '%s';
+$GLOBALS['TL_LANG']['tl_iso_address']['street_1'][0] = 'Straße, Hsnr';
+$GLOBALS['TL_LANG']['MSC']['next'] = '»';
+$GLOBALS['TL_LANG']['MSC']['previous'] = '«';
+if ($GLOBALS['TL_LANGUAGE'] == 'de'){
+    $GLOBALS['TL_LANG']['MSC']['more'] = '› Mehr erfahren';
+    $GLOBALS['TL_LANG']['MSC']['detailLabel'] = '› Mehr erfahren';
+}
+$GLOBALS['TL_LANG']['MSC']['keywords'] = 'Finden...';
