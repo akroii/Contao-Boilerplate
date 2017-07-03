65a89af0d0ded4577625a9de60376ad3951bec38
Andreas Kroll <andreas.kroll@yahoo.de>
2017-07-03 16:52:02 +0200

    Update langconfig.php

--- system/config/langconfig.php
+++ system/config/langconfig.php
@@ -1,11 +1,10 @@
 <?php
 
-$GLOBALS['TL_LANG']['MOD']['news'] = array('Blog');
+//$GLOBALS['TL_LANG']['MOD']['news'] = array('Blog');
 //$GLOBALS['TL_LANG']['MOD']['faq'] = array('Häufige Fragen');
 $GLOBALS['TL_LANG']['MSC']['com_submit'] = 'Absenden';
 $GLOBALS['TL_LANG']['MSC']['com_email'] = 'E-Mail';
 $GLOBALS['TL_LANG']['MSC']['copyOf'] = '%s';
-$GLOBALS['TL_LANG']['tl_iso_address']['street_1'][0] = 'Straße, Hsnr';
 $GLOBALS['TL_LANG']['MSC']['next'] = '»';
 $GLOBALS['TL_LANG']['MSC']['previous'] = '«';
 if ($GLOBALS['TL_LANGUAGE'] == 'de'){
